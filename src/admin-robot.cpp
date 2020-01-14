ccc22c1d1a1894ff040f48ee008753a87e60f668
Thomas CASPAR <thomas.caspar@outlook.com>
2020-01-14 13:23:56 +0100

    [INIT] Config End

---
+++ src/admin-robot.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : admin-robot.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
