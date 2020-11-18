20486cc9878d88ab20503525cc32a4b13a721e44
ThomasCaspar <thomas.caspar@outlook.com>
2020-11-18 15:05:41 +0100

    Update ODO_Odometrie.cpp

--- src/ODO/ODO_Odometrie.cpp
+++ src/ODO/ODO_Odometrie.cpp
@@ -7,7 +7,7 @@
 
 #include "ODO_Odometrie.hpp"
 
-ODO::COdometrie::COdometrie(COF::SStrategieDeplacement* p_strategieDeplacement, COD::CSerialCodeurManager* p_codeursManager)
+ODO::COdometrie::COdometrie(COF::CStrategieDeplacement* p_strategieDeplacement, COD::CSerialCodeurManager* p_codeursManager)
 {
 	m_odometrieStruct = {0};
 	m_strategieDepalcement = p_strategieDeplacement;
@@ -26,7 +26,22 @@ ODO::COdometrie::~COdometrie()
 
 void ODO::COdometrie::initialiser()
 {
+	m_odometrieStruct->xDepart = m_strategieDepalcement->getStrategieDeplacement(0)->x;
+	m_odometrieStruct->yDepart = m_strategieDepalcement->getStrategieDeplacement(0)->y;
+	m_odometrieStruct->yDepart = m_strategieDepalcement->getStrategieDeplacement(0)->y;
+	m_odometrieStruct->angleConsigne = m_strategieDepalcement->getStrategieDeplacement(0)->angle;
+}
 
+void ODO::COdometrie::reorientation()
+{
+	if (abs(m_odometrieStruct->angleConsigne - m_odometrieStruct->angleActuel) > 180) {
+		if (m_odometrieStruct->angleActuel > 0) {
+			m_odometrieStruct->angleActuel -= 360;
+		}
+		else if (orientation < 0) {
+			m_odometrieStruct->angleActuel += 360;
+		}
+	}
 }
 
 
