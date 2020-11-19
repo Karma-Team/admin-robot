07fc68f20d451999b30274e2c5681287aed660a7
ThomasCaspar <thomas.caspar@outlook.com>
2020-11-19 13:33:10 +0100

    Update ODO_Odometrie.cpp

--- src/ODO/ODO_Odometrie.cpp
+++ src/ODO/ODO_Odometrie.cpp
@@ -14,6 +14,7 @@ ODO::COdometrie::COdometrie(COF::CStrategieDeplacement* p_strategieDeplacement, 
 	m_strategieDepalcement = p_strategieDeplacement;
 	m_configStruct = p_configStruct;
 	m_codeursManager = p_codeursManager;
+	m_index = 0;
 
 	if (m_strategieDepalcement == NULL || m_codeursManager == NULL || m_configStruct == NULL) {
 		printf("Pointeur NULL !!!!!");
@@ -28,39 +29,10 @@ ODO::COdometrie::~COdometrie()
 
 void ODO::COdometrie::initialiser()
 {
-	m_odometrieStruct.xDepart = m_strategieDepalcement->getStrategieDeplacement(0)->x;
-	m_odometrieStruct.yDepart = m_strategieDepalcement->getStrategieDeplacement(0)->y;
-	m_odometrieStruct.angleConsigne = m_strategieDepalcement->getStrategieDeplacement(0)->angle;
+	m_odometrieStruct.xActuel = m_strategieDepalcement->getStrategieDeplacement(m_index)->x;
+	m_odometrieStruct.yActuel = m_strategieDepalcement->getStrategieDeplacement(m_index)->y;
+	m_odometrieStruct.angleConsigne = m_strategieDepalcement->getStrategieDeplacement(m_index)->angle;
 	m_odometrieStruct.vitesse = 0;
-}
-
-void ODO::COdometrie::reorientation()
-{
-	if (abs(m_odometrieStruct.angleConsigne - m_odometrieStruct.angleActuel) > 180) {
-		if (m_odometrieStruct.angleActuel > 0) {
-			m_odometrieStruct.angleActuel -= 360;
-		}
-		else if (m_odometrieStruct.angleActuel < 0) {
-			m_odometrieStruct.angleActuel += 360;
-		}
-	}
-}
-
-void ODO::COdometrie::calculConsigne()
-{
-   /* Pour savoir si la cible est à gauche ou à droite du robot, on
-	* calcule le sinus de l'angle entre la cible et le robot
-	* sin = Opp/Hyp, on a opp et on retrouve hyp grâce à Pythagore
-	* Sinus auxiliaire pour savoir où se trouve la cible par
-	* rapport au robot
-	*/
-	float sinAux = (m_odometrieStruct.yArrive - m_odometrieStruct.yDepart) / (sqrt((m_odometrieStruct.xArrive - m_odometrieStruct.xDepart) * (m_odometrieStruct.xArrive - m_odometrieStruct.xDepart) + (m_odometrieStruct.yArrive - m_odometrieStruct.yDepart) * (m_odometrieStruct.yArrive - m_odometrieStruct.yDepart)));
-	float aco = acos((m_odometrieStruct.xArrive - m_odometrieStruct.xDepart) / ((sqrt((m_odometrieStruct.xArrive - m_odometrieStruct.xDepart)*(m_odometrieStruct.xArrive - m_odometrieStruct.xDepart) + (m_odometrieStruct.yArrive - m_odometrieStruct.yDepart)*(m_odometrieStruct.yArrive - m_odometrieStruct.yDepart))))) * 360 /(2 * M_PI);
-
-	// On change la consigne selon le fait que la cible soit à gauche
-	// (-aco) ou a droite (+aco) du robot
-	m_odometrieStruct.angleConsigne = sinAux > 0 ? -aco : +aco;
-    reorientation();
 }
 
 void ODO::COdometrie::miseAJourPosition()
@@ -70,24 +42,37 @@ void ODO::COdometrie::miseAJourPosition()
 	m_odometrieStruct.nbTickDroit = m_codeursManager->getRightTicks();
 	m_odometrieStruct.nbTickGauche = m_codeursManager->getLeftTicks();
 
-	// Calcul de la différence du nombre de tic entre chaque codeur (car
-	// différent si on ne roule pas droit) qu'on converti en angle
-	m_odometrieStruct.orientation = (m_configStruct->getConfRobot().coeffAngleRoueGauche * m_odometrieStruct.nbTickGauche - m_configStruct->getConfRobot().coeffAngleRoueDroite * m_odometrieStruct.nbTickDroit) + m_odometrieStruct.orientationDerive;
-	m_odometrieStruct.orientationDerive = m_odometrieStruct.orientation;
-
+	// Calcul de l'orientation du robot actuel
+	m_odometrieStruct.orientationActuel = (m_configStruct->getConfRobot().coeffAngleRoueGauche * m_odometrieStruct.nbTickGauche - m_configStruct->getConfRobot().coeffAngleRoueDroite * m_odometrieStruct.nbTickDroit) + m_odometrieStruct.orientationDerive;
+	m_odometrieStruct.orientationDerive = m_odometrieStruct.orientationActuel;
 
 	// Calcul de la distance parcourue
 	m_odometrieStruct.distanceParcourue = (m_configStruct->getConfRobot().coeffLongueurRoueGauche * m_odometrieStruct.nbTickGauche + m_configStruct->getConfRobot().coeffLongueurRoueDroite * m_odometrieStruct.nbTickDroit) / 2;
 
-	// Calcul de la derive en vitesse
+	// Calcul de la vitesse de deplacement
 	m_odometrieStruct.vitesse = m_odometrieStruct.distanceParcourue - m_odometrieStruct.distancePrecedent;
 	m_odometrieStruct.distancePrecedent = m_odometrieStruct.distanceParcourue;
-
+	
+	// Actualisation de la position X et Y du robot
 	m_odometrieStruct.xDelta = -m_odometrieStruct.vitesse*(sin(m_odometrieStruct.orientation));
 	m_odometrieStruct.yDelta =  m_odometrieStruct.vitesse*(cos(m_odometrieStruct.orientation));
+	
+	m_odometrieStruct.xActuel = m_odometrieStruct.xActuel + m_odometrieStruct.xDelta;
+	m_odometrieStruct.yActuel = m_odometrieStruct.yActuel + m_odometrieStruct.yDelta;
+}
 
-	m_odometrieStruct.xArrive = m_odometrieStruct.xArrive + m_odometrieStruct.xDelta;
-	m_odometrieStruct.yArrive = m_odometrieStruct.yArrive + m_odometrieStruct.yDelta;
+void ODO::COdometrie::calculConsigneDeplacement()
+{
+	// Recuperation de la position de destination
+	m_odometrieStruct.xArrive = m_strategieDepalcement->getStrategieDeplacement(m_index)->x;
+	m_odometrieStruct.yArrive = m_strategieDepalcement->getStrategieDeplacement(m_index)->y;
+	
+	// Calcul de la distance qui separe le robot de la cible
+	m_odometrieStruct.distanceCible = sqrt(((m_odometrieStruct.xArrive - m_odometrieStruct.xActuel) * (m_odometrieStruct.xArrive - m_odometrieStruct.xActuel)) + ((m_odometrieStruct.yArrive - m_odometrieStruct.yActuel) * (m_odometrieStruct.yArrive - m_odometrieStruct.yActuel)));
+	
+	// Calcul de l'orientation du robot pour faire face a la cible
+	m_odometireStruct.orientationVersCible = arctg((m_odometrieStruct.xArrive - m_odometrieStruct.xActuel)/(m_odometrieStruct.yArrive - m_odometrieStruct.yActuel));
+	m_odometireStruct.orientationConsigne = m_odometireStruct.orientationVersCible - m_odometireStruct.orientationActuel;
 }
 
 
