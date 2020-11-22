884ff8a1da78192e1a64419b89bae3f9e5cfdcdb
thomas <thomas@thomas-N551VW>
2020-11-22 01:04:28 +0100

    Modification de la gestion de la liaison serie pour eviter les pertes de donnees

--- src/COD/COD_SerialCodeurManager.cpp
+++ src/COD/COD_SerialCodeurManager.cpp
@@ -69,114 +69,44 @@ void COD::CSerialCodeurManager::initialisation()
 
 void COD::CSerialCodeurManager::readAndReset()
 {
-	
-		int ticks[4];
 		char SerieData = ' ';
-		char tickChar [10000];
-		char tickd [10000];
-		char tickg [10000];
-		int dataAvail = 0;
-		int i = 0;
-		int s = 0;
-		bool G = false;
-		bool D = false;
+		char SerilDataTab[10000] = {0};
+		char tickGauche[1000] = {0};
+		char tickDroit[1000] = {0};
 
-		bool G1 = false;
-		bool D1 = false;
+		int index = 0;
 
-		int g1 = 0, d1 = 0, g2 = 0, d2 = 0;
-
-		memset(tickChar, ' ', 10000);
-		memset(tickd, ' ', 10000);
-		memset(tickg, ' ', 10000);
-
-		fflush (stdout) ;
 		serialPutchar (fd, 'C') ;
-
-	  	/*if (millis () > nextTime)
-		{
-			//Debuguer
-			//printf ("\nOut: %3d: ", count) ;
-		    fflush (stdout) ;
-		    serialPutchar (fd, 'C') ;
-		    nextTime += 10 ;
-		    ++count ;
-		}*/
-
-		//delay (10) ;
 
 		while (serialDataAvail (fd))
 		{
-			SerieData = serialGetchar (fd);
-			tickChar[i] = SerieData;
-			//Debuguer
-		    //printf (" -> %c", serialGetchar (fd)) ;
-		    if(SerieData == '?')
-		    {
-		    	G = true;
-		    	D = false;
-		    }
-		    if(SerieData == '!')
-		    {
-		    	D = true;
-		    	G = false;
-		    }
-
-		    if(G == true && D == false)
-		    {
-		    	if(G1 == false)
-		    		g2++;
-		    	if(g2 == 2)
-		    	{
-		    		G1 = true;
-		    		tickg[g1] = SerieData;
-		    		g1++;
-
-		    	}
-		    		
-		    }
-		    if(G == false && D == true)
-		    {
-		    	if(D1 == false)
-		    		d2++;
-		    	if(d2 == 2)
-		    	{
-		    		D1 = true;
-		    		tickd[d1] = SerieData;
-		    		d1++;
-		    	}
-		    }
-
-		    i++;
-		    fflush (stdout) ;
+			SerilDataTab[index] = serialGetchar (fd);
+			fflush (stdout) ;
+			index++;
 		}
-
 		
-		i = 0, g1 = 0, d1 = 0, g2 = 0, d2 = 0;
-
-		
-		ticks[0] =  atoi(tickd);
-		ticks[2] =  atoi(tickd);
-		ticks[1] =  atoi(tickg);
-		ticks[3] =  atoi(tickg);
-
-		//Debuguer
-		//printf ("\n"); 		
-		
-		//cout << " G:" << ticks[0] <<" D:" << ticks[1] << " G:" << ticks[2] << " D:" << ticks[3] << endl;
-		
-		
-		if(ticks[0] == ticks[2] && ticks[1] == ticks[3])	
+		for(int i = 1; i < index; i++)
 		{
-			m_leftTicks = ticks[0];
-			m_rightTicks = ticks[1];
-			
+			if(SerilDataTab[index] != '\0')
+			{
+				if(SerilDataTab[index] != '!')
+				{
+					strcpy(tickGauche, &SerilDataTab[index]);
+				}
+				else
+				{
+					strcpy(tickDroit, &SerilDataTab[index]);
+				}
+			}
+			else
+			{
+				break;
+			}
 		}
-		else
-		{
-			m_leftTicks = 0;
-			m_rightTicks = 0;
-		}
+
+		m_rightTicks = atoi(tickDroit);
+		m_leftTicks = atoi(tickGauche);
+
 		reset();
 }
 
