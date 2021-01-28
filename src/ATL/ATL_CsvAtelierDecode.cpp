a5d853544b228e2535822f1be01b6a78b08cb1bc
ThomasCaspar <thomas.caspar@outlook.com>
2021-01-28 15:10:27 +0100

    Update ATL_CsvAtelierDecode.cpp

--- src/ATL/ATL_CsvAtelierDecode.cpp
+++ src/ATL/ATL_CsvAtelierDecode.cpp
@@ -43,37 +43,62 @@ ATL::CCsvAtelierDecode::CCsvAtelierDecode()
 	m_index = 0;
 }
 
-void ATL::CCsvAtelierDecode::readCsv(char * p_csvAtelierFile, MOT::CMoteurPWM* p_moteurManager,  ODO::COdometrie* p_odometrie)
+void ATL::CCsvAtelierDecode::readCsv(char * p_csvAtelierFile)
 {
-	if (p_csvAtelierFile == NULL || p_moteurManager == NULL || p_odometrie == NULL)
+	if (p_csvAtelierFile == NULL || p_csvAtelierFile == "null")
 	{
-		printf("ERREUR un des pointeurs fournie est null p_csvAtelierFile %d p_moteurManager %d p_odometrie %d, p_csvAtelierFile, p_odometrie, p_odometrie);
+		printf("ERREUR un des pointeurs fournie est null p_csvAtelierFile %s ", p_csvAtelierFile);
 		exit(1);
 	}
-		       
-	SSV::CSerialServoApi serialServoApi = SSV::CSerialServoApi(COF::CConfigurationRobot::inst()->getConfRobot()->servoSerieTty, 115200);
 
 	io::CSVReader<9> in(p_csvAtelierFile);
 	in.read_header(io::ignore_extra_column, "idServo", "ModeServo", "Angle/Vitesse", "TimeoutServo", "xDeplacement", "yDeplacement", "AngleDeplacement", "VitesseDeplacement", "timeoutAtelier");
+	m_index = 0;
 	
 	while(in.read_row(m_scsvAtelierAction.id, m_scsvAtelierAction.modeServo, m_scsvAtelierAction.vitesseAngleServo, m_scsvAtelierAction.timeoutServo, m_scsvAtelierAction.xDeplacement, m_scsvAtelierAction.yDeplacement, m_scsvAtelierAction.angleDeplacement, m_scsvAtelierAction.vitesseDeplacement, m_scsvAtelierAction.timeoutAtelier))
 	{
+		m_scsvAtelierActionTab[index].id = m_scsvAtelierAction.id;
+		m_scsvAtelierActionTab[index].modeServo = m_scsvAtelierAction.modeServo;
+		m_scsvAtelierActionTab[index].vitesseAngleServo = m_scsvAtelierAction.vitesseAngleServo;
+		m_scsvAtelierActionTab[index].xDeplacement = m_scsvAtelierAction.xDeplacement;
+		m_scsvAtelierActionTab[index].yDeplacement = m_scsvAtelierAction.yDeplacement;
+		m_scsvAtelierActionTab[index].angleDeplacement = m_scsvAtelierAction.angleDeplacement;
+		m_scsvAtelierActionTab[index].vitesseDeplacement = m_scsvAtelierAction.vitesseDeplacement;
+		m_scsvAtelierActionTab[index].timeoutAtelier = m_scsvAtelierAction.timeoutAtelier;
+		m_index++;
+	}
+}
+
+bool ATL::CCsvAtelierDecode::lancerAtelier(MOT::CMoteurPWM* p_moteurManager,  ODO::COdometrie* p_odometrie)
+{
+	if (p_moteurManager == NULL || p_odometrie == NULL)
+	{
+		printf("ERREUR un des pointeurs fournie est null p_moteurManager %d p_odometrie %d", p_moteurManager, p_odometrie);
+		exit(1);
+	}
+	
+	SSV::CSerialServoApi serialServoApi = SSV::CSerialServoApi(COF::CConfigurationRobot::inst()->getConfRobot()->servoSerieTty, 115200);
+	
+	int index = 0;
+	
+	while(index != m_index)
+	{
 		uint32_t timeout = 0;
-		std::thread threadSeralServoAtelier (threadActionneurAtelier,&serialServoApi, &m_scsvAtelierAction.modeServo, &m_scsvAtelierAction.id, &m_scsvAtelierAction.vitesseAngleServo, &m_scsvAtelierAction.timeoutServo);
+		std::thread threadSeralServoAtelier (threadActionneurAtelier,&serialServoApi, &m_scsvAtelierActionTab[index].modeServo, &m_scsvAtelierActionTab[index].id, &m_scsvAtelierActionTab[index].vitesseAngleServo, &m_scsvAtelierActionTab[index].timeoutServo);
 		threadSeralServoAtelier.detach(); // Execution du thread sans bloquer le thread principal
-		
-		SStrategieDeplacement strategieDeplacementAtelier = {m_scsvAtelierAction.xDeplacement, m_scsvAtelierAction.yDeplacement, m_scsvAtelierAction.angleDeplacement, m_scsvAtelierAction.vitesseDeplacement, "null"};
-		std::thread threadAsservDeplacmentAtelier (threadDeplacementAtelier, p_moteurManager, p_odometrie, &strategieDeplacementAtelier, &m_scsvAtelierAction.timeoutAtelier);
+
+		SStrategieDeplacement strategieDeplacementAtelier = {m_scsvAtelierActionTab[index].xDeplacement, m_scsvAtelierActionTab[index].yDeplacement, m_scsvAtelierActionTab[index].angleDeplacement, m_scsvAtelierActionTab[index].vitesseDeplacement, "null"};
+		std::thread threadAsservDeplacmentAtelier (threadDeplacementAtelier, p_moteurManager, p_odometrie, &strategieDeplacementAtelier, &m_scsvAtelierActionTab[index].timeoutAtelier);
 		threadAsservDeplacmentAtelier.detach(); // Execution du thread sans bloquer le thread principal
 		
-		while(timeout < m_scsvAtelierAction.timeoutAtelier ) // Attente de la fin de l'action de la ligne de l'atelier pour passer a la suivante
+		while(timeout < m_scsvAtelierActionTab[index].timeoutAtelier ) // Attente de la fin de l'action de la ligne de l'atelier pour passer a la suivante
 		{
 			timeout++;
 			usleep(1 * 1000);
 		}
+		index++;
 	}
 }
-
 
 
 void ATL::CCsvAtelierDecode::threadActionneurAtelier(SSV::CSerialServoApi* p_serialServoApi, char* p_modeServo, uint32_t* p_id, double* p_vitesseAngleServo, uint32_t* p_timeoutServo)
